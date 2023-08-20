6d4c889bd24c467e682ec1ffc1326b27293dbc3e
kdnakt <a.kid.1985@gmail.com>
2023-08-21 05:17:00 +0900

    handle connections one by one

--- simple-web-server/src/main.rs
+++ simple-web-server/src/main.rs
@@ -1,5 +1,21 @@
-use std::net::TcpListener;
+use std::net::{
+    TcpListener,
+    TcpStream,
+};
+use std::io;
 
 fn main() {
     let listener = TcpListener::bind("localhost:3000").unwrap();
+
+    loop {
+        let (connection, _) = listener.accept().unwrap();
+
+        if let Err(e) = handle_connection(connection) {
+            println!("failed to handle connection: {e}");
+        }
+    }
+}
+
+fn handle_connection(connection: TcpStream) -> io::Result<()> {
+    Ok(())
 }
