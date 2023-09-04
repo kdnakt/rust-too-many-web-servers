a39fa7cfd17fe5cb5330104216e000a406fb425f
kdnakt <a.kid.1985@gmail.com>
2023-09-05 01:57:35 +0900

    use EPOLL_CTL_ADD flag

--- 04-multiplexed-server/src/main.rs
+++ 04-multiplexed-server/src/main.rs
@@ -1,3 +1,17 @@
+use epoll::{
+    Event,
+    Events,
+    ControlOptions::*,
+};
+use std::os::fd::AsRawFd;
+use std::net::TcpListener;
+
 fn main() {
+    let listener = TcpListener::bind("localhost:3000").unwrap();
+    listener.set_nonblocking(true).unwrap();
+
     let epoll = epoll::create(false).unwrap();
+
+    let event = Event::new(Events::EPOLLIN, listener.as_raw_fd() as _);
+    epoll::ctl(epoll, EPOLL_CTL_ADD, listener.as_raw_fd(), event).unwrap();
 }
