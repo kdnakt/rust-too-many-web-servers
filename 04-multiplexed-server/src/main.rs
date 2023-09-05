d0aced258f1494f3bec0f9612daa7102bbf15b90
kdnakt <a.kid.1985@gmail.com>
2023-09-06 00:21:56 +0900

    epoll::wait()

--- 04-multiplexed-server/src/main.rs
+++ 04-multiplexed-server/src/main.rs
@@ -5,6 +5,7 @@ use epoll::{
 };
 use std::os::fd::AsRawFd;
 use std::net::TcpListener;
+use std::io;
 
 fn main() {
     let listener = TcpListener::bind("localhost:3000").unwrap();
@@ -14,4 +15,26 @@ fn main() {
 
     let event = Event::new(Events::EPOLLIN, listener.as_raw_fd() as _);
     epoll::ctl(epoll, EPOLL_CTL_ADD, listener.as_raw_fd(), event).unwrap();
+
+    loop {
+        let mut events = [Event::new(Events::empty(), 0); 1024];
+        let timeout = -1; // block forever, until something happens
+        let num_events = epoll::wait(epoll, timeout, &mut events).unwrap();
+
+        for event in &events[..num_events] {
+            let fd = event.data as i32;
+            // is the listener ready?
+            if fd == listener.as_raw_fd() {
+                // try accepting a connection
+                match listener.accept() {
+                    Ok((connection, _)) => {
+                        connection.set_nonblocking(true).unwrap();
+                        // TODO
+                    }
+                    Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                    Err(e) => panic!("{e}"),
+                }
+            }
+        }
+    }
 }
