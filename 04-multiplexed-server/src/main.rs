2ff5dcb967dec077945b42d0635feb2fdf720cf2
kdnakt <a.kid.1985@gmail.com>
2023-09-07 01:18:04 +0900

    store connections and states in HashMap

--- 04-multiplexed-server/src/main.rs
+++ 04-multiplexed-server/src/main.rs
@@ -6,6 +6,19 @@ use epoll::{
 use std::os::fd::AsRawFd;
 use std::net::TcpListener;
 use std::io;
+use std::collections::HashMap;
+
+enum ConnectionState {
+    Read {
+        request: [u8; 1024],
+        read: usize
+    },
+    Write {
+        response: &'static [u8],
+        written: usize,
+    },
+    Flush,
+}
 
 fn main() {
     let listener = TcpListener::bind("localhost:3000").unwrap();
@@ -16,12 +29,13 @@ fn main() {
     let event = Event::new(Events::EPOLLIN, listener.as_raw_fd() as _);
     epoll::ctl(epoll, EPOLL_CTL_ADD, listener.as_raw_fd(), event).unwrap();
 
+    let mut connections = HashMap::new();
     loop {
         let mut events = [Event::new(Events::empty(), 0); 1024];
         let timeout = -1; // block forever, until something happens
         let num_events = epoll::wait(epoll, timeout, &mut events).unwrap();
 
-        for event in &events[..num_events] {
+        'next: for event in &events[..num_events] {
             let fd = event.data as i32;
             // is the listener ready?
             if fd == listener.as_raw_fd() {
@@ -32,11 +46,20 @@ fn main() {
                         // register the connection with epoll
                         let event = Event::new(Events::EPOLLIN | Events::EPOLLOUT, fd as _);
                         epoll::ctl(epoll, EPOLL_CTL_ADD, fd, event).unwrap();
+
+                        let state = ConnectionState::Read {
+                            request: [0u8; 1024],
+                            read: 0,
+                        };
+                        connections.insert(fd, (connection, state));
                     }
                     Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
                     Err(e) => panic!("{e}"),
                 }
+                continue 'next;
             }
+            // otherwise, a connection must be ready
+            let (connection, state) = connections.get_mut(&fd).unwrap();
         }
     }
 }
