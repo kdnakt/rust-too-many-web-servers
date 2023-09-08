2aabe8d2447808098c12b810e9b239bf31f6f1e3
kdnakt <a.kid.1985@gmail.com>
2023-09-09 00:52:34 +0900

    task is a future

--- 05-futures/src/main.rs
+++ 05-futures/src/main.rs
@@ -2,6 +2,12 @@
 // fn spawn<T: Task>(task: T);
 trait Task {}
 
+trait Future {
+    type Output;
+
+    fn run(&mut self) -> Option<Self::Output>;
+}
+
 fn main() {
     println!("Hello, world!");
 }
