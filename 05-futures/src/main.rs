20905bd65dc05291f74ed2a390122be5d4c3b686
kdnakt <a.kid.1985@gmail.com>
2023-09-10 01:23:49 +0900

    register I/O resources with epoll

--- 05-futures/src/main.rs
+++ 05-futures/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 // fn spawn<T: Task>(task: T);
 trait Task {}
@@ -9,11 +10,24 @@ trait Future {
 }
 
 impl Scheduler {
+    fn spawn<T>(&self, mut future: T) {
+        let id = rand();
+        // poll the future once to get it started, passing in it's ID
+        future.poll(event.id);
+        // store the future
+        self.tasks.insert(id, future);
+    }
+
     fn run(&self) {
-        loop {
-            for future in &self.tasks {
-                future.poll();
-            }
+        // loop {
+        //     for future in &self.tasks {
+        //         future.poll();
+        //     }
+        // }
+        for event in epoll_events {
+            // poll the future associated with this event
+            let future = self.tasks.get(&event.id).unwrap();
+            future.poll(event.id);
         }
     }
 }
