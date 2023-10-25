ebec84967cfcd41c0b9e3316d77efb952ef9aa87
kdnakt <a.kid.1985@gmail.com>
2023-10-25 10:19:06 +0900

    fn ctrl_c() to detect the signal

--- 06-graceful-server/src/main.rs
+++ 06-graceful-server/src/main.rs
@@ -1,3 +1,11 @@
+use signal_hook::consts::signal::SIGINT;
+use signal_hook::iterator::Signals;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn ctrl_c() {
+    let mut signal = Signals::new(&[SIGINT]).unwrap();
+    let _ctrl_c = signal.forever().next().unwrap(); // blocks the thread...
 }
