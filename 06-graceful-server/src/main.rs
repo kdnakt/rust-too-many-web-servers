0a9bd311b129e688c47c159dbb3a5679286f8b73
kdnakt <a.kid.1985@gmail.com>
2023-10-26 13:35:42 +0900

    add basic building blocks

--- 06-graceful-server/src/main.rs
+++ 06-graceful-server/src/main.rs
@@ -1,3 +1,6 @@
+use std::sync::{
+    Arc,
+};
 use signal_hook::consts::signal::SIGINT;
 use signal_hook::iterator::Signals;
 
@@ -8,4 +11,43 @@ fn main() {
 fn ctrl_c() {
     let mut signal = Signals::new(&[SIGINT]).unwrap();
     let _ctrl_c = signal.forever().next().unwrap(); // blocks the thread...
+}
+
+fn spawn_blocking(blocking_work: impl FnOnce() + Send + 'static) -> impl Future<Output = ()> {
+    poll_fn(|waker| {
+        // TODO
+        None
+    })
+}
+
+#[derive(Clone)]
+struct Waker(Arc<dyn Fn() + Send + Sync>);
+
+impl Waker {
+    fn wake(&self) {
+        (self.0)()
+    }
+}
+
+trait Future {
+    type Output;
+
+    fn poll(&mut self, waker: Waker) -> Option<Self::Output>;
+}
+
+fn poll_fn<F, T>(f: F) -> impl Future<Output = T>
+where F: FnMut(Waker) -> Option<T>,
+{
+    struct PollFn<F>(F);
+
+    impl<F, T> Future for PollFn<F>
+    where F: FnMut(Waker) -> Option<T>,
+    {
+        type Output = T;
+        fn poll(&mut self, waker: Waker) -> Option<Self::Output> {
+            (self.0)(waker)
+        }
+    }
+
+    PollFn(f)
 }
