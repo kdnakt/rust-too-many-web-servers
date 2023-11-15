ec94a9b552ab647838d0a523d1c0bf78c3ab1256
kdnakt <a.kid.1985@gmail.com>
2023-11-15 10:05:07 +0900

    start of final web server

--- tokio-server/src/main.rs
+++ tokio-server/src/main.rs
@@ -1,5 +1,25 @@
+use tokio::net::{
+    TcpListener,
+    TcpStream,
+};
+use std::io;
+
 // Spins up the runtime and runs the async code in main
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
+
+    let listener = TcpListener::bind("localhost:3000").await.unwrap();
+
+    loop {
+        let (connection, _) = listener.accept().await.unwrap();
+
+        if let Err(e) = handle_connection(connection).await {
+            println!("failed to handle connection: {e}");
+        }
+    }
+}
+
+async fn handle_connection(mut connection: TcpStream) -> io::Result<()> {
+    Ok(())
 }
